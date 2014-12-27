e6cd826253d2589f545480fdc65894dd85f7b7b8
unknown <git@github.com>
2014-12-27 18:09:33 +0800

    m

--- js/common.js
+++
@@ -1,6 +0,0 @@
-function menu_status(status) {
-	var menua = document.getElementById("display_menu");
-	var menub = document.getElementById("pop_menu");
-	menua.style.display = status;
-	menub.style.display = status;
-}
