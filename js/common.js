16919908451996ad5ea2f0dacf96c5f658a57d2e
unknown <git@github.com>
2014-12-27 20:22:03 +0800

    m

--- js/common.js
+++ js/common.js
@@ -1,35 +1,31 @@
-(function($, _aoWin) {
+$(document).ready(function(){
 
-	if($("img.guide").length > 0) {
-		var _nTimer = 0,
-			_oGuide$ = $(".guide"),
-			_oGuideTrigger$ = $("#guideTrigger, #tipTrigger"),
-			_oMask$ = $(".mask");
+	var _trigger = $("#guideTrigger, #tipTrigger"),
+		_guide = $(".guide"),
+		_mask = $(".mask"),
+		_timer = 0;
 
-			function _back() {
-				_nTimer = setTimeout(function() {
-				_oMask$.stop().animate({opacity:0}, function(){$(".mask").hide()});
-				_oGuide$.stop().animate({marginLeft:"-120px",opacity:0}, "400", "swing",function(){
-					_oGuide$.hide();
-				});
+		function _back() {
+			_timer = setTimeout(function() {
+			_mask.stop().animate({opacity:0}, function(){$(".mask").hide()});
+			_guide.stop().animate({marginLeft:"-120px",opacity:0}, "400", "swing",function(){
+				_guide.hide();
+			});
 			}, 100);
 		}
+	
+	_guide.css({"left":"50%", "opacity":0});
+	_trigger.css({"backgroundColor":"white", "opacity":"0"});
+	_trigger.mouseover(function(){
+		clearTimeout(_timer);
+		_mask.show().stop().animate({"opacity":0.2});
+		_guide.css("display","block").stop().animate({marginLeft:"+168px", opacity:1}, 900, "swing", function() {
+			_guide.animate({marginLeft:"+153px"}, 300);
+		});
+	}).mouseout(_back);
 
-		/*guide*/
-		_oGuide$.css({"left":"50%", "opacity":0});
-		_oGuideTrigger$.css({"backgroundColor":"white", "opacity":"0"});
-		_oGuideTrigger$.mouseover(function(){
-			clearTimeout(_nTimer);
-			_oMask$.show().stop().animate({"opacity":0.2});
-			_oGuide$.css("display", "block").stop().animate({marginLeft:"+168px", opacity:1}, 900, "swing", function() {
-				_oGuide$.animate({marginLeft:"+153px"}, 300);
-			});
-			_ossLog();
-		}).mouseout(_back);
+	_guide.mouseover(function(){
+		clearTimeout(_timer);
+	}).mouseout(_back);
 
-		_oGuide$.mouseover(function(){
-			clearTimeout(_nTimer);
-		}).mouseout(_back);
-	}
-
-})(jQuery, window);
+});
